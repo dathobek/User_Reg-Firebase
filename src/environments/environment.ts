4f790da97bf565c07bad9461ca32c039d1fa4074
David Athobe <dathobek@gmail.com>
2018-06-10 01:34:06 +0100

    firebase-installation

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBeQPghuPXu-MLjbJtXKvfkTtU7v897gpE",
+    authDomain: "registration-firebase-348d6.firebaseapp.com",
+    databaseURL: "https://registration-firebase-348d6.firebaseio.com",
+    projectId: "registration-firebase-348d6",
+    storageBucket: "registration-firebase-348d6.appspot.com",
+    messagingSenderId: "747375975767"
+  }
 };
